24d752a73043c49cc1b02a8d861f550333517afb
hzb2 <hzb2a@meitu.com>
2021-09-03 18:58:00 +0800

    demo

---
+++ src/main/index.js
@@ -0,0 +1,75 @@
+import { app, BrowserWindow } from 'electron'; // eslint-disable-line
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+// if (process.env.NODE_ENV !== 'development') {
+//   global.__static = require('path')
+//     .join(__dirname, '/static')
+//     .replace(/\\/g, '\\\\'); // eslint-disable-line
+// }
+
+let mainWindow;
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:9080'
+    : `file://${__dirname}/index.html`;
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true, // 是否启用节点集成
+      nodeIntegrationInWorker: true, // 是否在Web工做器中启用了Node集成
+      contextIsolation: false, // electron为12x版本新增此行
+      devTools: true, // 是否开启 DevTools
+      // webSecurity: true//是否禁用同源策略(上线删除)
+    },
+  });
+
+  mainWindow.loadURL(winURL);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'elec-demo',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
